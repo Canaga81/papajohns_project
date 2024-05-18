be705f65ab3be8d906d62a3b5f84392de418db31
Canaga81 <Canagamemmedov@gmail.com>
2024-05-18 15:17:40 +0400

    Pizza filter component responsive style finish

--- src/components/PizzaFilter/PizzaFilter.jsx
+++ src/components/PizzaFilter/PizzaFilter.jsx
@@ -1,4 +1,4 @@
-const PizzaFilter = ( {items, setFilteredData} ) => {
+const PizzaFilter = ( { items, setFilteredData } ) => {
 
     const filterByCategory = (category) => {
         const filteredData = items.filter(item => item.cath && item.cath.includes(category));
@@ -12,12 +12,12 @@ const PizzaFilter = ( {items, setFilteredData} ) => {
   return (
     <div className='h-[90px] leading-[90px] bg-promoBg '>
         <div className='container mx-auto'>
-            <div className='flex items-center pl-6 gap-3'>
-                <h3 onClick={filterByAll} className='text-[18px] cursor-pointer transition-all hover:text-red-500'>Hamısı</h3>
-                <h3 onClick={() => filterByCategory('chick')} className='text-[18px] cursor-pointer transition-all hover:text-red-500'>Toyuqlu</h3>
-                <h3 onClick={() => filterByCategory('meat')} className='text-[18px] cursor-pointer transition-all hover:text-red-500'>Ət ilə</h3>
-                <h3 onClick={() => filterByCategory('vegan')} className='text-[18px] cursor-pointer transition-all hover:text-red-500'>Vegetarian</h3>
-                <h3 onClick={() => filterByCategory('spicy')} className='text-[18px] cursor-pointer transition-all hover:text-red-500'>Acılı</h3>
+            <div className='flex items-center sm:pl-6 rd:pl-2 rd:pr-2 pl-1 pr-0 sm:gap-3 gap-1.5'>
+                <h3 onClick={filterByAll} className='sm:text-[24px] rd:text-[18px] text-[16px] cursor-pointer transition-all hover:text-red-500 font-bold'>Hamısı</h3>
+                <h3 onClick={() => filterByCategory('chick')} className='sm:text-[24px] rd:text-[18px] text-[16px] cursor-pointer transition-all hover:text-red-500 font-bold'>Toyuqlu</h3>
+                <h3 onClick={() => filterByCategory('meat')} className='sm:text-[24px] rd:text-[18px] text-[16px] cursor-pointer transition-all hover:text-red-500 font-bold'>Ət ilə</h3>
+                <h3 onClick={() => filterByCategory('vegan')} className='sm:text-[24px] rd:text-[18px] text-[16px] cursor-pointer transition-all hover:text-red-500 font-bold'>Vegetarian</h3>
+                <h3 onClick={() => filterByCategory('spicy')} className='sm:text-[24px] rd:text-[18px] text-[16px] cursor-pointer transition-all hover:text-red-500 font-bold'>Acılı</h3>
             </div>
         </div>
     </div>
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -5,7 +5,7 @@ const Promo = () => {
     <div className='h-[90px] leading-[90px] bg-promoBg mt-[25px] sm:mr-9 mr-3 sm:ml-9 ml-3'>
         <div className='container mx-auto'>
             <div className='flex items-center justify-center gap-9 px-5'>
-                <h2 className='hidden cr:text-3xl cr:block font-bold'>Promo kodunuz var? <span>Endirimdən istifadə edin!</span> </h2>
+                <h2 className='hidden cr:text-3xl cr:block font-bold'>Promo kodunuz var? <span className='text-aquaBg'>Endirimdən istifadə edin!</span> </h2>
                 <div className='flex h-[90px] items-center gap-3'>
                     <input className='h-[39px] leading-[39px] p-2 bg-white outline-none border border-aquaBg rounded-lg w-full' type="text" placeholder='Promo kodu daxil edin' />
                     <button className='h-[39px] text-[18px] rounded font-bold leading-[39px] inline-block px-[21px] bg-aquaBg border-none outline-none cursor-pointer text-white transition-all hover:opacity-80'>OK</button>
